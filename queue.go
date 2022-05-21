87e4faf4055292ab122bcd3c9660a0b2206f65de
colindickson <colin.dickson@gmail.com>
2022-05-21 08:29:18 -0400

    generic container/heap implementation

--- queue.go
+++ queue.go
@@ -1,17 +1,48 @@
 package priority
 
+//generic implementation of priority queue example found at https://pkg.go.dev/container/heap#example-package-PriorityQueue
+
 type Item[T any] struct {
-	value    T
+	Value T
+
 	priority int
 	index    int
 }
 
 type Queue[T any] []*Item[T]
 
-func (q Queue[T]) Get() (*Item[T], error) {
-	return nil, nil
+func (q Queue[T]) Len() int {
+	return len(q)
 }
 
-func (q Queue[T]) Add(i *Item[T]) error {
-	return nil
+func (q Queue[T]) Less(i, j int) bool {
+	return q[i].priority > q[j].priority
+}
+
+func (q Queue[T]) Swap(i, j int) {
+	q[i], q[j] = q[j], q[i]
+	q[i].index = i
+	q[j].index = j
+}
+
+func (q *Queue[T]) Push(item *Item[T]) {
+	n := len(*q)
+	item.index = n
+	*q = append(*q, item)
+}
+
+func (q *Queue[T]) Pop() *Item[T] {
+	old := *q
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	item.index = -1
+	*q = old[0 : n-1]
+	return item
+}
+
+func (q *Queue[T]) Update(item *Item[T], value T, priority int) {
+	item.Value = value
+	item.priority = priority
+	Fix[*Item[T]](q, item.index)
 }
---
+++ queue_test.go
@@ -0,0 +1,41 @@
+package priority
+
+import (
+	"fmt"
+)
+
+func Example() {
+	// Some items and their priorities.
+	items := map[string]int{
+		"banana": 3, "apple": 2, "pear": 4,
+	}
+
+	// Create a priority queue, put the items in it, and
+	// establish the priority queue (heap) invariants.
+	pq := make(Queue[string], len(items))
+	i := 0
+	for value, priority := range items {
+		pq[i] = &Item[string]{
+			Value:    value,
+			priority: priority,
+			index:    i,
+		}
+		i++
+	}
+	Init[*Item[string]](&pq)
+
+	// Insert a new item and then modify its priority.
+	item := &Item[string]{
+		Value:    "orange",
+		priority: 1,
+	}
+	Push[*Item[string]](&pq, item)
+
+	pq.Update(item, item.Value, 5)
+
+	// Take the items out; they arrive in decreasing priority order.
+	for pq.Len() > 0 {
+		item := Pop[*Item[string]](&pq)
+		fmt.Printf("%.2d:%s ", item.priority, item.Value)
+	}
+}
